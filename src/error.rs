263008eee5bd562f4796c7ff50655ca00d7e576f
Nugine <nugine@foxmail.com>
2020-08-14 15:26:26 +0800

    Fix S3Error

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,18 @@
+use crate::s3_path::ParseS3PathError;
 use crate::utils::BoxStdError;
 
 use std::error::Error;
 use std::fmt::{self, Display};
 
-// TODO: switch to thiserror, see https://github.com/dtolnay/thiserror/issues/79
+// TODO: switch to thiserror
+// See https://github.com/dtolnay/thiserror/issues/79
 
 pub type S3Result<T, E = NopError> = Result<T, S3Error<E>>;
 
 #[derive(Debug)]
 pub enum S3Error<E = NopError> {
     Operation(E),
-    InvalidRequest(BoxStdError), // TODO: switch to a specific error type
+    InvalidRequest(InvalidRequestError),
     InvalidOutput(InvalidOutputError),
     Storage(BoxStdError),
     NotSupported,
@@ -18,6 +20,13 @@ pub enum S3Error<E = NopError> {
 
 #[derive(Debug, Clone, Copy)]
 pub struct NopError;
+
+#[derive(Debug, thiserror::Error)]
+pub enum InvalidRequestError {
+    #[error(transparent)]
+    ParsePath(#[from] ParseS3PathError),
+    // FIXME: add other errors
+}
 
 #[derive(Debug, thiserror::Error)]
 pub enum InvalidOutputError {
@@ -55,7 +64,7 @@ impl<E: Error + 'static> Error for S3Error<E> {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
             Self::Operation(e) => Some(e),
-            Self::InvalidRequest(e) => Some(e.as_ref()),
+            Self::InvalidRequest(e) => Some(e),
             Self::InvalidOutput(e) => Some(e),
             Self::Storage(err) => Some(err.as_ref()),
             Self::NotSupported => None,
