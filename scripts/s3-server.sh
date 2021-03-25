fcbd503d9ee6738bd39f468eb59f11a66ee34ee4
Nugine <nugine@foxmail.com>
2021-03-25 21:17:19 +0800

    Move binary

--- scripts/s3-server.sh
+++ scripts/s3-server.sh
@@ -4,14 +4,10 @@ if [ -n "$1" ]; then
 	DATA_DIR="$1"
 fi
 
-if [ -z "$RUST_LOG" ]; then 
-    export RUST_LOG=info,s3_server=debug,s3=debug
-fi
-
 RELEASE=""
 RELEASE="--release"
 
-cargo run $RELEASE --example s3 \
+cargo run $RELEASE --features binary \
     -- \
     --access-key AKIAIOSFODNN7EXAMPLE \
     --secret-key wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY \
