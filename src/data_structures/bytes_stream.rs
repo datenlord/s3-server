e1508b479ff6ebe39c1eb211d73841b91b619abd
Nugine <nugine@foxmail.com>
2022-03-15 20:24:26 +0800

    Add byte stream limit

--- src/data_structures/bytes_stream.rs
+++ src/data_structures/bytes_stream.rs
@@ -1,8 +1,8 @@
 //! `BytesStream`
 
-use std::io;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use std::{io, mem};
 
 use futures::stream::Stream;
 use hyper::body::Bytes;
@@ -13,13 +13,20 @@ pin_project! {
         #[pin]
         reader: R,
         buf_size: usize,
+        buf: Vec<u8>,
+        limit: Option<usize>,
     }
 }
 
 impl<R> BytesStream<R> {
     /// Constructs a `BytesStream`
-    pub const fn new(reader: R, buf_size: usize) -> Self {
-        Self { reader, buf_size }
+    pub const fn new(reader: R, buf_size: usize, limit: Option<usize>) -> Self {
+        Self {
+            reader,
+            buf_size,
+            buf: Vec::new(),
+            limit,
+        }
     }
 }
 
@@ -27,20 +34,27 @@ impl<R: futures::AsyncRead> Stream for BytesStream<R> {
     type Item = io::Result<Bytes>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        // FIXME: reuse the buf
-        let mut buf = vec![0_u8; self.buf_size];
-
         let this = self.project();
 
-        let ret: io::Result<usize> = futures::ready!(this.reader.poll_read(cx, &mut buf));
+        let buf_len = match *this.limit {
+            Some(lim) => lim.min(*this.buf_size),
+            None => *this.buf_size,
+        };
+        this.buf.resize(buf_len, 0);
+
+        let ret: io::Result<usize> = futures::ready!(this.reader.poll_read(cx, this.buf));
         let ans: Option<io::Result<Bytes>> = match ret {
+            Ok(n) if n == 0 => None,
             Ok(n) => {
-                if n == 0 {
-                    None
-                } else {
-                    buf.truncate(n);
-                    Some(Ok(buf.into()))
+                let nread = n.min(buf_len);
+                this.buf.truncate(nread);
+                let buf = Bytes::from(mem::take(this.buf));
+
+                if let Some(ref mut lim) = *this.limit {
+                    *lim = lim.wrapping_sub(nread);
                 }
+
+                Some(Ok(buf))
             }
             Err(e) => Some(Err(e)),
         };
